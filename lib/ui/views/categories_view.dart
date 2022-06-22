dc977e100ad98cfb16dc15da0e9f40aa362dea8a
Le127 <nereocarbonara@gmail.com>
2022-06-22 16:23:45 -0300

    video 145

--- lib/ui/views/categories_view.dart
+++ lib/ui/views/categories_view.dart
@@ -1,12 +1,19 @@
+import 'package:admin_dashboard/ui/buttons/custom_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:admin_dashboard/datatables/categories_datasource.dart';
 import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 
-
-
 //La View default de panel administrativo para crear otras View
-class CategoriesView extends StatelessWidget {
+class CategoriesView extends StatefulWidget {
   const CategoriesView({Key? key}) : super(key: key);
+
+  @override
+  State<CategoriesView> createState() => _CategoriesViewState();
+}
+
+class _CategoriesViewState extends State<CategoriesView> {
+  int _rowsPerPage = PaginatedDataTable.defaultRowsPerPage;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -22,6 +29,16 @@ class CategoriesView extends StatelessWidget {
             DataColumn(label: Text('Acciones')),
           ],
           source: CategoriesDTS(),
+          header: const Text('Categorías disponibles', maxLines: 2),
+          onRowsPerPageChanged: (value) {
+            _rowsPerPage = value ?? 10;
+            setState(() {});
+          },
+          rowsPerPage: _rowsPerPage,
+          actions: [
+            CustomIconButton(
+                onPressed: () {}, text: 'Crear', icon: Icons.add_outlined)
+          ],
         )
       ],
     );
